c333418edcc386e7316cb43bbdd8e5cbb4cb70c7
Miguel Matey Sanz <al341802@uji.es>
2023-06-26 10:50:38 +0200

    feat(hu01): add controller implementation

--- app/src/main/java/com/ei1039_1048/notesapp/NotesController.kt
+++ app/src/main/java/com/ei1039_1048/notesapp/NotesController.kt
@@ -2,17 +2,21 @@ package com.ei1039_1048.notesapp
 
 import com.ei1039_1048.notesapp.data.Note
 import com.ei1039_1048.notesapp.data.NoteRepository
+import com.ei1039_1048.notesapp.exceptions.EmptyTitleException
 import kotlinx.coroutines.flow.Flow
 
 class NotesController(
     private val notesRepository: NoteRepository
 ) {
 
-    fun getNotesStream(): Flow<List<Note>> {
-        throw NotImplementedError("TODO")
-    }
+    fun getNotesStream(): Flow<List<Note>> = notesRepository.getNotesStream()
 
-    fun createNote(title: String, description: String) {
-        throw NotImplementedError("TODO")
+    suspend fun createNote(title: String, description: String) {
+        if (title.isEmpty()) {
+            throw EmptyTitleException()
+        }
+
+        val note = Note(title = title, description = description)
+        notesRepository.insert(note)
     }
 }
