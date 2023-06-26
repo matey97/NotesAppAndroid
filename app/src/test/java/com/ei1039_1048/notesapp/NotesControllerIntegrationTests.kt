d72fa3ec4263be328703f04509296d5e2584f492
Miguel Matey Sanz <al341802@uji.es>
2023-06-26 11:00:05 +0200

    test(hu02): add integration tests

--- app/src/test/java/com/ei1039_1048/notesapp/NotesControllerIntegrationTests.kt
+++ app/src/test/java/com/ei1039_1048/notesapp/NotesControllerIntegrationTests.kt
@@ -3,6 +3,7 @@ package com.ei1039_1048.notesapp
 import com.ei1039_1048.notesapp.data.Note
 import com.ei1039_1048.notesapp.data.NoteRepository
 import com.ei1039_1048.notesapp.exceptions.EmptyTitleException
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -29,8 +30,24 @@ class NotesControllerIntegrationTests {
     private lateinit var notesController: NotesController
 
     private val emptyTitle = ""
+    private val id1 = "id1"
     private val title1 = "Test title 1"
     private val description1 = "Test description 1"
+    private val id2 = "id2"
+    private val title2 = "Test title 2"
+    private val description2 = "Test description 2"
+
+    private val note1 = Note(
+        id = id1,
+        title = title1,
+        description = description1
+    )
+
+    private val note2 = Note(
+        id = id2,
+        title = title2,
+        description = description2
+    )
 
     @Before
     fun setup() {
@@ -63,5 +80,39 @@ class NotesControllerIntegrationTests {
         notesController.createNote(emptyTitle, description1)
 
         // Then: se lanza la excepción EmptyTitleException
+    }
+
+    @Test
+    fun HU02_E01() = runBlocking {
+        // Given: no hay ninguna nota
+        whenever(noteRepositoryMock.getNotesStream()).thenReturn(flow {
+            emit(emptyList())
+        })
+
+        // When: se consultan las notas
+        val notes = notesController.getNotesStream().first()
+
+        // Then: se obtiene una lista vacía (del mock)
+        verify(noteRepositoryMock).getNotesStream()
+        assertEquals(0, notes.size)
+    }
+
+    @Test
+    fun HU02_E02() = runBlocking {
+        // Given: hay varias notas almacenadas
+        whenever(noteRepositoryMock.getNotesStream()).thenReturn(flow {
+            emit(listOf(note1, note2))
+        })
+
+        // When: se consultan las notas
+        val notes = notesController.getNotesStream().first()
+
+        // Then: se obtiene una lista con las notas almacenadas (del mock)
+        verify(noteRepositoryMock).getNotesStream()
+        assertEquals(2, notes.size)
+        assertEquals(title1, notes[0].title)
+        assertEquals(description1, notes[0].description)
+        assertEquals(title2, notes[1].title)
+        assertEquals(description2, notes[1].description)
     }
 }
